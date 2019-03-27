a6c160676e811058dd0dbc37ab55a4c80cdf56cb
dbZrg <40048447+dbZrg@users.noreply.github.com>
2019-03-27 21:08:37 +0100

    Update 3.c

--- Vjezba3/3.c
+++ Vjezba3/3.c
@@ -3,32 +3,39 @@
 
 int main() {
 	int N;
+	
 	printf("Upisi N:");
 	scanf("%d", &N);
+	
 	int *arr = (int*)malloc(sizeof(int)*N);
-	for (int i = 0, z = N; i < N; i++,z--) {
-		*(arr+i) = z;
-		printf("%d", *(arr + i));
+	int *koraka_do_sad= (int*)malloc(sizeof(int)*N);
+	int index2 = 0, index3 = 0, broj_koraka = 0;
+	
+	*(koraka_do_sad) = 0;
+	for (int i = 1; i < N; i++) {
+		if (*(arr + i) % 2 == 0) {  // provjeri jeli djeljiv sa 2
+				index2 = *(arr + i)/2;   // ako je nadji mu index tj lokaciju
+				broj_koraka = *(koraka_do_sad + index2-1);  // spremi broj koraka 
+		}
+		if (*(arr + i) % 3 == 0) {  // provjeri jeli djeljiv sa 3
+				index3 = *(arr + i) / 2;  // ako je nadji mu index tj lokaciju
+				// min od broj koraka za N/2 i broja koraka za N/3
+				broj_koraka = (broj_koraka > *(koraka_do_sad + index3-1)) ? *(koraka_do_sad + index3-1) : broj_koraka; 
+				
+		}
+		// min od min broj koraka do sada i broja koraka za jedan pomak unazad
+		broj_koraka = (broj_koraka > *(koraka_do_sad + i -1)) ? *(koraka_do_sad + i -1) : broj_koraka;
+
+		//dobijemo min broj koraka od 3 moguca slucaja te dodamo u listu koraka_do_sad
+		*(koraka_do_sad + i) = broj_koraka + 1;
+	
 	}
-	int i = 0;
-	int koraci = 0;
-	while (i < N-1) {
-		if (*(arr+i) % 2 == 0) {
-			i += *(arr+i) / 2;
-			koraci++;
-			continue;
-		}
-		else if (*(arr+i) % 3 == 0) {
-			i += *(arr+i) / 3;
-			koraci++;
-			continue;
-		}
-		else {
-			i++;
-			koraci++;
-		}
-	}
-	printf("koraci: %d",koraci);
+
+		//zadnji element liste je rjesenje
+		printf("%d ", *(koraka_do_sad + N-1));
+
 	getchar();
 	getchar();
+	free(arr);
+	free(koraka_do_sad);
 }
